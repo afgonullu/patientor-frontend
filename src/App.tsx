6262fd5787e4d375aab60fab9f47a80fa661cd58
A Faruk Gonullu <af@gonul.lu>
2020-12-26 15:06:55 +0300

    FSO: end of exercise 9.11

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,31 @@
-import React from "react";
-import axios from "axios";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
-import { Button, Divider, Header, Container } from "semantic-ui-react";
+import React from "react"
+import axios from "axios"
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom"
+import { Button, Divider, Header, Container } from "semantic-ui-react"
 
-import { apiBaseUrl } from "./constants";
-import { useStateValue } from "./state";
-import { Patient } from "./types";
+import { apiBaseUrl } from "./constants"
+import { useStateValue } from "./state"
+import { Patient } from "./types"
 
-import PatientListPage from "./PatientListPage";
+import PatientListPage from "./PatientListPage"
 
 const App: React.FC = () => {
-  const [, dispatch] = useStateValue();
+  const [, dispatch] = useStateValue()
   React.useEffect(() => {
-    axios.get<void>(`${apiBaseUrl}/ping`);
+    axios.get<void>(`${apiBaseUrl}/ping`)
 
     const fetchPatientList = async () => {
       try {
         const { data: patientListFromApi } = await axios.get<Patient[]>(
-          `${apiBaseUrl}/patients`
-        );
-        dispatch({ type: "SET_PATIENT_LIST", payload: patientListFromApi });
+          `${apiBaseUrl}/api/patients`
+        )
+        dispatch({ type: "SET_PATIENT_LIST", payload: patientListFromApi })
       } catch (e) {
-        console.error(e);
+        console.error(e)
       }
-    };
-    fetchPatientList();
-  }, [dispatch]);
+    }
+    fetchPatientList()
+  }, [dispatch])
 
   return (
     <div className="App">
@@ -42,7 +42,7 @@ const App: React.FC = () => {
         </Container>
       </Router>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
